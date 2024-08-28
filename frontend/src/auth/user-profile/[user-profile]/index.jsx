c4603a9e70d395785dbdb74a752a0b6f8b7ba3d2
Rida-Lamiini <ridalamiini0@gmail.com>
2024-08-28 01:38:40 +0100

    add sign in

--- frontend/src/auth/user-profile/[user-profile]/index.jsx
+++ frontend/src/auth/user-profile/[user-profile]/index.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
-import UserInformation from './components/UserInformation'
+// import React from 'react'
+// import UserInformation from './components/UserInformation'
+
+// function UserProfile() {
+
+
+
+
+//   return (
+//     <div>
+//         {/* user information */}
+//         <UserInformation/>
+//         {/* commande information */}
+//         {/* lists */}
+//     </div>
+//   )
+// }
+
+// export default UserProfile
+
+import React from 'react';
+import { useAuth } from './AuthContext';
 
 function UserProfile() {
+  const { user, signOut } = useAuth();
+
+  if (!user) {
+    return <div>Please sign in</div>;
+  }
 
   return (
     <div>
-        {/* user information */}
-        <UserInformation/>
-        {/* commande information */}
-        {/* lists */}
+      <h1>Welcome, {user.firstName} {user.lastName}</h1>
+      <p>Email: {user.email}</p>
+      <button onClick={signOut}>Sign Out</button>
     </div>
-  )
+  );
 }
 
-export default UserProfile
+export default UserProfile;

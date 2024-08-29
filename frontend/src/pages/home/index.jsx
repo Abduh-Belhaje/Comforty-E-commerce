6d345790e1cd2d2cccdd985d0617c48319686487
Rida-Lamiini <ridalamiini0@gmail.com>
2024-08-29 12:56:04 +0100

    delete some file

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,6 @@
-import { useUser } from '@clerk/clerk-react';
-import React from 'react'
+import { useUser } from "@clerk/clerk-react";
+import React from "react";
 
 export default function Home() {
-
-  
-
-  return (
-    <div>Home</div>
-  )
+  return <div>Home</div>;
 }

6c99f152e1c99e637faf51f96066b991e8b2c015
ali ouarrirh <aouarrirh@gmail.com>
2024-08-30 22:08:15 +0100

    added the home Page carousel

---
+++ frontend/src/pages/home/components/BannerCarousel.jsx
@@ -0,0 +1,25 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+function BannerCarousel() {
+  return (
+    <div>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
+
+export default BannerCarousel;

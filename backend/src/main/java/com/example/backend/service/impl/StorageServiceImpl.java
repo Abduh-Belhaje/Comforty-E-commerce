ed13cab314296ce531deabe74148400eeffd8d4f
Abduh-Belhaje <abduhbelhaj@gmail.com>
2024-09-01 19:01:13 +0100

    upload files to Amazon S3

--- backend/src/main/java/com/example/backend/service/impl/StorageServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/impl/StorageServiceImpl.java
@@ -6,7 +6,6 @@ import com.example.backend.service.StorageService;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;

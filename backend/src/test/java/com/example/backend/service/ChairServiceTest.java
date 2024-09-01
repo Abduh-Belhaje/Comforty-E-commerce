abfa3ca60e570a6be5fba31863c09690ac4f3d55
Abduh-Belhaje <abduhbelhaj@gmail.com>
2024-09-01 18:59:19 +0100

    test the add chair method

--- backend/src/test/java/com/example/backend/service/ChairServiceTest.java
+++ backend/src/test/java/com/example/backend/service/ChairServiceTest.java
@@ -7,6 +7,7 @@ import org.mapstruct.factory.Mappers;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.backend.dto.chair.AddChairDTO;
 import com.example.backend.exception.FailedAddingChairException;
@@ -16,11 +17,14 @@ import com.example.backend.mapper.ChairMapper;
 import com.example.backend.model.Chair;
 import com.example.backend.repository.CategoryRepository;
 import com.example.backend.repository.ChairRepository;
+import com.example.backend.repository.ImageRepository;
 import com.example.backend.service.impl.ChairServiceImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
 
 @ExtendWith(MockitoExtension.class)
 public class ChairServiceTest {
@@ -37,17 +41,23 @@ public class ChairServiceTest {
         private CategoryRepository categoryRepository;
 
         @Mock
+        private ImageRepository imageRepository;
+
+        @Mock
+        private MultipartFile file;
+
+        @Mock
         private ChairMapper chairMapper = Mappers.getMapper(ChairMapper.class);
 
         @BeforeEach
         public void setup() {
                 chairService = new ChairServiceImpl(chairRepository, categoryRepository,
-                                storageService, chairMapper);
+                                storageService, imageRepository, chairMapper);
         }
 
         @Test
         void shouldAddNewChair() throws UploadFileException, FileConvertingException, FailedAddingChairException {
-                AddChairDTO chairDTO = new AddChairDTO("xx", "xx", null, "ooo", "red", "10",
+                AddChairDTO chairDTO = new AddChairDTO("xx", "xx", new ArrayList<MultipartFile>(), "ooo", "red", "10",
                                 "10");
 
                 given(chairMapper.toEntity(chairDTO))
@@ -56,15 +66,11 @@ public class ChairServiceTest {
                 given(categoryRepository.findCategoryByName(chairDTO.getCategoty()))
                                 .willReturn(1L);
 
-                // given(storageService.uploadFile(chairDTO.getImage()))
-                // .willReturn("http://image_url");
-
                 chairService.addChair(chairDTO);
 
                 ArgumentCaptor<Chair> ChairArgumentCaptor = ArgumentCaptor.forClass(Chair.class);
                 verify(chairRepository).save(ChairArgumentCaptor.capture());
                 assertThat(ChairArgumentCaptor.getValue().getCtg_id()).isEqualTo(1L);
-                // assertThat(ChairArgumentCaptor.getValue().getPicture_url()).isEqualTo("http://image_url");
 
         }
 }

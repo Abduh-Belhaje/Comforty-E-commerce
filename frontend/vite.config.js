88592ac77e165c5af5975f72eda204598f8f4c67
Rida-Lamiini <ridalamiini0@gmail.com>
2024-09-02 16:22:09 +0100

    v1 image for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  outDir: "dist", // This should match the COPY path in the Dockerfile
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});

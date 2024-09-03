8a1ad583f40df1dc6522d8941f5f90972eda0543
ali ouarrirh <aouarrirh@gmail.com>
2024-09-03 02:30:59 +0100

    added the feature

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,11 +1,17 @@
 import BannerCarousel from "./components/bannerCarousel";
+import Feature from "../../assets/Feature.png";
 import { useUser } from "@clerk/clerk-react";
 import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <BannerCarousel />
-    </div>
+    <>
+      <div className="flex flex-col">
+        <BannerCarousel />
+      </div>
+      <div className="absolute flex justify-center align-center border-black ">
+        <img src={Feature} className="border border-black " />
+      </div>
+    </>
   );
 }

840b1beeb36b7410c2ae007db102cba437c0ab64
Abduh-Belhaje <abduhbelhaj@gmail.com>
2024-09-05 13:06:22 +0100

    contain database objects

---
+++ db/init.sql
@@ -0,0 +1,21 @@
+INSERT INTO catalog.categories (ctg_id,name,description,created_at)
+VALUES (1,'Office','Designed to provide comfort and support for long hours of work.',NOW());
+
+INSERT INTO catalog.categories (ctg_id,name,description,created_at)
+VALUES (2,'Dining','Luxurious chairs often made from leather and designed for high-level executives.',NOW());
+
+INSERT INTO catalog.categories (ctg_id,name,description,created_at)
+VALUES (3,'Healthcare','Chairs that can be easily folded and stored, ideal for small spaces or additional seating.',NOW());
+
+INSERT INTO catalog.categories (ctg_id,name,description,created_at)
+VALUES (4,'Outdoor','Chairs that recline backward and often have a footrest, providing maximum relaxation.',NOW());
+
+INSERT INTO catalog.categories (ctg_id,name,description,created_at)
+VALUES (5,'Living Room ','Chairs designed to support children while studying or doing homework.',NOW());
+
+
+-- select * from catalog.categories;
+
+-- DELETE FROM catalog.categories WHERE ctg_id IN (1,2,3,4,5)
+
+

e39d6a1188be24cbb2b5b0ab5b9c5975cf6f1b04
Rida-Lamiini <ridalamiini0@gmail.com>
2024-09-05 14:51:10 +0100

    fix import bug

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,7 +13,6 @@ import { UserProvider } from "./contexte/UserContext.jsx";
 import { Toaster } from "@/components/ui/sonner";
 import Products from "./pages/products/index.jsx";
 import UserProfile from "./auth/user-profile/[user-profile]/index.jsx";
-import Product from "./pages/products/product/[productId]/index.jsx";
 import ProductPage from "./pages/products/product/[productId]/index.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
